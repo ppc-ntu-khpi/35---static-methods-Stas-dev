cf32ece6228dea1043fa6f205ab3ede8d48d102f
Stanislav Utkin <stasytkin0000@gmail.com>
2021-01-27 22:28:23 +0200

    Readme changed, programm is ready, mb markdown troubles:

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,53 @@
 package domain;
+import java.util.ArrayList;
 
+
+// Завдання -  знайти усі прості дільники заданого шестизначного числа
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    
+    public static String  Calculate(int number ){ 
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        String outline = " " ;
+        foo(number, primeNumbers);
+        
+        for(int j =0; j<primeNumbers.size(); j++){
+        
+        outline += "  " + Integer.toString(primeNumbers.get(j));
+        
+        }
+        
+       
+        
+        
+      
+        return outline;
+        
+       
+      }
+    
+    public static ArrayList<Integer> foo (int number,  ArrayList<Integer> primeNumbers ){
+    
+    int ctr = 0;
+   
+   
+    for(int  i  = number - 1; i!=1 ; i--){
+        ctr = 0;
+        if(number%i == 0 ){
+             for(int j = i -1; j !=1; j--){
+                  if(i % j == 0){
+                   ctr ++; 
+                  } 
+            }
+                if(ctr == 0 ){
+                    primeNumbers.add(i);
+                
+            }
+        } 
+    
+     }
+   return  primeNumbers;  
     }
-}
+   
+
+ 
+}    
